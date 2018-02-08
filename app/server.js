7614e65c1e35a4188d470229079cf132d1018cc9
Pawel Maricnkowski <wafel34@gmail.com>
2018-02-08 20:54:02 +0000

    General set up for the project

--- app/server.js
+++ app/server.js
@@ -1,10 +1,19 @@
-var express = require('express');
+'use strict';
+
+const express = require('express');
 const app = express();
+
+app.set('PORT', (process.env.PORT || 3000));
+app.set('view engine', 'ejs');
+app.set('views', './app');
+
+app.use(express.static('app/public'));
+
 app.get('/', (req, res) => {
-    res.send('siema');
+    res.render('index');
 });
 
 
-app.listen(3000, () => {
-    console.log('App listening on port: ');
+app.listen(app.get('PORT'), () => {
+    console.log('App listening on port: ' + app.get('PORT'));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // imports
 
 const gulp = require('gulp');
