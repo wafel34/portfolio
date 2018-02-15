fd38818cabb57be0f4a4b87ec5f14f6afd693536
Pawel Marcinkowski <pmarcinkowski1016@timeinc.com>
2018-02-15 13:34:13 +0000

    Working on svg animations

--- resources/scripts/animations.js
+++ resources/scripts/animations.js
@@ -1,12 +1,24 @@
-function animateIntro() {
+var animations = {};
+
+animations.animateIntro = function() {
     var tl = new TimelineMax();
     tl.from('.console', 1, {transform: 'scale(0)', ease: Elastic.easeOut.config(1, 0.5), delay: 0.5});
-    //tl.staggerFrom('.console-text', 1, {transform: 'scaleX(0)'});
+    tl.staggerTo('#mypath', 3, {
+        attr: {
+            d: 'M15 350 h500'
+        }
+    });
+};
 
-
-}
-
-function animateScreen() {
+animations.animateScreen = function () {
     TweenLite.to('.screen-rectangle', 1, {width: 400, transform: 'translateX(400px)', ease: Power2.easeInOut});
     TweenLite.to('.bottom', 0.5, {opacity: 0, ease: Power4.easeInOut});
-}
+};
+
+var controller = new ScrollMagic.Controller();
+new ScrollMagic.Scene({
+    triggerElement: "#about"
+})
+    .on('start', function(){animations.animateScreen();})
+    .setClassToggle('#about', 'dupa') // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -14,6 +14,6 @@ $(document).ready(function(){
             });
         });
 
-        animateIntro();
+        animations.animateIntro();
 
 });
